6ad69b2d12c63a65b9947e033ac8a099497cda6e
sebacapu29 <sebacapu3@gmail.com>
2021-05-24 18:36:19 -0300

    creo pagina de login y registro

---
+++ tp_pps/src/app/pages/login/login.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  miFormulario:FormGroup;
+
+  constructor(private formBuilder:FormBuilder) { 
+    this.miFormulario = formBuilder.group({
+      email: new FormControl(""),
+      clave: new FormControl("")
+    })
+  }
+
+  ngOnInit() {
+  }
+
+  Autenticar(form){
+  }
+}
