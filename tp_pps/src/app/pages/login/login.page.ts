918e0bbe1ae3c75f31f9302434233846c3ddbe9e
attrix <lucho.bernal@hotmail.com>
2021-05-25 14:42:09 -0300

    backend login

--- tp_pps/src/app/pages/login/login.page.ts
+++ tp_pps/src/app/pages/login/login.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from './../../shared/User.class';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,18 +11,49 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class LoginPage implements OnInit {
 
-  miFormulario:FormGroup;
+  miFormulario: FormGroup;
 
-  constructor(private formBuilder:FormBuilder) { 
+  @Input() user: User = new User();
+
+  public ingresando: boolean;
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private authSvc: AuthService) {
     this.miFormulario = formBuilder.group({
       email: new FormControl(""),
-      clave: new FormControl("")
+      clave: new FormControl(""),
     })
+
+    this.user.email = '';
+    this.user.password = '';
+    this.ingresando = false;
   }
 
   ngOnInit() {
   }
 
-  autenticar(form){
+  autenticar(form) {
   }
+
+
+  async onLogin() {
+    this.user.email = this.miFormulario.value.email;
+    this.user.password = this.miFormulario.value.clave;
+    const user = await this.authSvc.onLogin(this.user);
+    if (user) {
+      this.authSvc.currentUser = this.user;
+      console.log(this.authSvc.currentUser);
+      this.router.navigateByUrl('/home');
+    }
+  }
+
+  public submit() {
+    //console.log(this.registrationForm.value);
+  }
+
+
+
+
+
+
+
 }
--- tp_pps/src/environments/environment.prod.ts
+++ tp_pps/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBBznS230Pr1sASgGCp9kutXs27MwKTORM",
+    authDomain: "pps-la-comanda-utndevs.firebaseapp.com",
+    projectId: "pps-la-comanda-utndevs",
+    storageBucket: "pps-la-comanda-utndevs.appspot.com",
+    messagingSenderId: "24906307675",
+    appId: "1:24906307675:web:b0d7d61e28466e3b15477e"
+  }
 };
