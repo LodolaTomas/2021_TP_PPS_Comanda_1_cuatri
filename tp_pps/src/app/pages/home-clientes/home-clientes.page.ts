bcf1a6aebaba1e9c55f8a390428a4f44656eefb0
sebacapu29 <sebacapu3@gmail.com>
2021-06-20 20:19:02 -0300

    agrego logica de cliente lee codigo qr y se inserta en la db de espera, agrego metodo para filtrar en la db

--- tp_pps/src/app/pages/home-clientes/home-clientes.page.ts
+++ tp_pps/src/app/pages/home-clientes/home-clientes.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { CloudFirestoreService } from 'src/app/services/cloud-firestore.service';
 
 @Component({
   selector: 'app-home-clientes',
@@ -10,9 +12,30 @@ import { AuthService } from 'src/app/services/auth.service';
 export class HomeClientesPage implements OnInit {
 
   navigate : any;
+  encodedData: any;
+  scannedBarCode: {};
+  input: any;
+  cliente:any;
+  displayQREspera:boolean=true;
 
-  constructor(private authS:AuthService, private router:Router) {
-    this.sideMenu();
+  //barcodeScannerOptions: BarcodeScannerOptions;
+
+  constructor(private authS:AuthService, 
+              private router:Router, 
+              private scanner: BarcodeScanner,
+              private fbService:CloudFirestoreService) {
+      this.sideMenu();
+      this.getUser();
+  }
+  async getUser(){
+    // const state = this.router.getCurrentNavigation().extras.state;
+    // if (state != null) {
+      // const idUser = state.value;
+      const idUser = "edb4z9BC3llciFBhEomB";
+      const fbCollection = await this.fbService.GetByParameter("usuarios","id",idUser).get().toPromise();
+      fbCollection.docs.forEach(d=> this.cliente = d.data());
+      console.log(fbCollection.docs[0].data());
+    //}
   }
   ngOnInit() {
   }
@@ -25,18 +48,39 @@ export class HomeClientesPage implements OnInit {
         // url   : "/alta-producto",
         icon  : "qr-code"
       }
-      // ,
-      
-        // {
-        //   title : "Alta de mesa",
-        //   url   : "/alta-mesa",
-        //   icon  : "pencil"
-        // }
+      ,
+        {
+          title : "Historial Encuesta",
+          // url   : "/alta-mesa",
+          icon  : "book-outline"
+        }
     ]
   }
   logout()
   {
     this.authS.LogOutCurrentUser()
     this.router.navigateByUrl('login')
+  }
+  openQR() {
+    console.log("QR!")
+    this.scanner.scan({ formats: "PDF_417" }).then(res => {
+      this.scannedBarCode = res;
+      console.log(res);
+      let scannedCode = res.text
+
+      // let userQR = scannedCode.split("@");
+      // console.log(userQR)
+      const userWaitList = { id:this.cliente.id, status:"esperando" };
+      this.fbService.Insert("lista_espera_local",userWaitList)
+                    .then((val)=>{
+                      alert("Agregado a la lista de espera!");
+                    });
+      this.displayQREspera=false;
+      this.input = this.scannedBarCode["text"];
+      // alert(this.input)
+    }).catch(err => {
+      alert(err);
+    });
+
   }
 }
