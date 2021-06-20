55820d60a89ad5edbb479f6c237b0275ab82d91e
sebacapu29 <sebacapu3@gmail.com>
2021-06-20 20:29:49 -0300

    logica insertar cliente en lista de espera

--- tp_pps/src/app/pages/home-clientes/home-clientes.page.ts
+++ tp_pps/src/app/pages/home-clientes/home-clientes.page.ts
@@ -28,14 +28,15 @@ export class HomeClientesPage implements OnInit {
       this.getUser();
   }
   async getUser(){
-    // const state = this.router.getCurrentNavigation().extras.state;
-    // if (state != null) {
-      // const idUser = state.value;
-      const idUser = "edb4z9BC3llciFBhEomB";
-      const fbCollection = await this.fbService.GetByParameter("usuarios","id",idUser).get().toPromise();
+    // this.authS.GetCurrentUser().then((response) => {
+    //   let userId =  response.uid;
+    // });
+
+      var currentUser = {uid:"edb4z9BC3llciFBhEomB"};
+      // var currentUser = await this.authS.GetCurrentUser();
+      const fbCollection = await this.fbService.GetByParameter("usuarios","id",currentUser.uid).get().toPromise();
       fbCollection.docs.forEach(d=> this.cliente = d.data());
       console.log(fbCollection.docs[0].data());
-    //}
   }
   ngOnInit() {
   }
@@ -67,9 +68,6 @@ export class HomeClientesPage implements OnInit {
       this.scannedBarCode = res;
       console.log(res);
       let scannedCode = res.text
-
-      // let userQR = scannedCode.split("@");
-      // console.log(userQR)
       const userWaitList = { id:this.cliente.id, status:"esperando" };
       this.fbService.Insert("lista_espera_local",userWaitList)
                     .then((val)=>{
@@ -77,7 +75,6 @@ export class HomeClientesPage implements OnInit {
                     });
       this.displayQREspera=false;
       this.input = this.scannedBarCode["text"];
-      // alert(this.input)
     }).catch(err => {
       alert(err);
     });
