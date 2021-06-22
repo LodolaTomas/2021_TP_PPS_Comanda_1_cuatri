63a783903611d412ce38d3ad843c9461b1f67a1b
attrix <lucho.bernal@hotmail.com>
2021-06-22 12:39:14 -0300

    admUsers

--- tp_pps/src/app/pages/supervisor/adm-usuarios/adm-usuarios.page.ts
+++ tp_pps/src/app/pages/supervisor/adm-usuarios/adm-usuarios.page.ts
@@ -18,7 +18,15 @@ export class AdmUsuariosPage implements OnInit {
   public verAceptados:boolean = false;
   public verTodos:boolean = true;
 
+  public buttonColor1: string = "dark";
+  public buttonColor2: string = "dark";
+  public buttonColor3: string = "light";
+
+  
+
   constructor(private authS: AuthService, private router: Router, private firestore: CloudFirestoreService) {
+
+
 
     firestore.GetAll("usuarios")
     .subscribe((data) => {
@@ -29,12 +37,36 @@ export class AdmUsuariosPage implements OnInit {
   }
 
 
+
+  seleccionarFiltro(tipo: string) {
+
+    switch (tipo) {
+      case 'pendientes':
+        this.buttonColor1 = "light";
+        this.buttonColor2 = "dark";
+        this.buttonColor3 = "dark";
+        break;
+      case 'aceptados':
+        this.buttonColor1 = "dark";
+        this.buttonColor2 = "light";
+        this.buttonColor3 = "dark";
+        break;
+      case 'todos':
+        this.buttonColor1 = "dark";
+        this.buttonColor2 = "dark";
+        this.buttonColor3 = "light";
+        break;
+
+    }
+  }
+
   verAceptadosBTN(){
     if(this.verAceptados)
     {
       this.verAceptados =false;
     }
     else{
+      this.seleccionarFiltro('aceptados')
       this.verAceptados =true;
       this.verPendientes =false;
       this.verTodos =false;
@@ -48,6 +80,7 @@ export class AdmUsuariosPage implements OnInit {
       this.verTodos =false;
     }
     else{
+      this.seleccionarFiltro('todos')
       this.verAceptados =false;
       this.verPendientes =false;
       this.verTodos =true;
@@ -61,6 +94,7 @@ export class AdmUsuariosPage implements OnInit {
       this.verPendientes =false;
     }
     else{
+      this.seleccionarFiltro('pendientes')
       this.verAceptados =false;
       this.verPendientes =true;
       this.verTodos =false;
