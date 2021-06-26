013470cd0b828c9926d0f9a6ed89d3433a5511f5
LodolaTomas <tomaslodola1@gmail.com>
2021-06-25 22:51:11 -0300

    login register fix

--- tp_pps/src/app/pages/login/login.page.ts
+++ tp_pps/src/app/pages/login/login.page.ts
@@ -3,6 +3,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { CloudFirestoreService } from 'src/app/services/cloud-firestore.service';
+import Swal, { SweetAlertIcon } from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -17,15 +19,15 @@ export class LoginPage implements OnInit {
 
   public ingresando: boolean;
 
-  users =[
-    {"email":"admin@yopmail.com","clave":"123456"},
-    {"email":"metre@yopmail.com","clave":"123456"},
-    {"email":"cliente@yopmail.com","clave":"123456"},
-    {"email":"cocinero@yopmail.com","clave":"123456"},
-    {"email":"supervisor@yopmail.com","clave":"123456"}
+  users = [
+    { "email": "admin@yopmail.com", "clave": "123456" },
+    { "email": "metre@yopmail.com", "clave": "123456" },
+    { "email": "cliente@yopmail.com", "clave": "123456" },
+    { "email": "cocinero@yopmail.com", "clave": "123456" },
+    { "email": "supervisor@yopmail.com", "clave": "123456" }
   ];
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private authSvc: AuthService) {
+  constructor(private formBuilder: FormBuilder, private router: Router, private authSvc: AuthService, private cloudSrv: CloudFirestoreService) {
     this.miFormulario = formBuilder.group({
       email: new FormControl(""),
       clave: new FormControl(""),
@@ -48,27 +50,33 @@ export class LoginPage implements OnInit {
   async onLogin() {
     this.user.email = this.miFormulario.value.email;
     this.user.password = this.miFormulario.value.clave;
-    const user = await this.authSvc.onLogin(this.user);
-    if (user) {
-      this.authSvc.currentUser = this.user;
-      console.log(this.authSvc.currentUser);
-      
-      if(this.user.email == 'supervisor@yopmail.com')
-      {
-        this.router.navigateByUrl('/supervisor');
-      }
-      else{
-        this.router.navigateByUrl('/home');
-      }
+    const miUser = await this.cloudSrv.GetByParameter('usuarios', 'correo', this.user.email).valueChanges().subscribe(async element=>{
+    if (element[0].estado == 'pendiente') {
+      this.alert('warning', 'Su cuenta esta pendiente')
+    } else if (element[0].estado == 'rechazado') {
+      this.alert('error', 'Su cuenta ah sido rechazada')
+    } else {
+      let user = await this.authSvc.onLogin(this.user);
+      if (user) {
+        this.authSvc.currentUser = this.user;
 
+        if (this.user.email == 'supervisor@yopmail.com') {
+          this.router.navigateByUrl('/supervisor');
+        }
+        else {
+          this.router.navigateByUrl('/home');
+        }
+
+      }
     }
+  })
   }
 
   public submit() {
     //console.log(this.registrationForm.value);
   }
 
-  public LoginFast(id:number){
+  public LoginFast(id: number) {
 
     this.miFormulario.controls['email'].setValue(this.users[id].email);
     this.miFormulario.controls['clave'].setValue(this.users[id].clave);
@@ -76,7 +84,25 @@ export class LoginPage implements OnInit {
   }
 
 
+  alert(icon: SweetAlertIcon, text: string) {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'bottom',
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
 
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+
+    Toast.fire({
+      icon: icon,
+      title: text
+    })
+  }
 
 
 
