88599609adc5848fc95c390445f28e103c6a57eb
LodolaTomas <tomaslodola1@gmail.com>
2021-06-30 19:44:00 -0300

    cosas

--- tp_pps/src/app/component/cartfood/cartfood.page.ts
+++ tp_pps/src/app/component/cartfood/cartfood.page.ts
@@ -1,18 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { ModalController } from '@ionic/angular';
+import { CloudFirestoreService } from 'src/app/services/cloud-firestore.service';
 @Component({
   selector: 'app-cartfood',
   templateUrl: './cartfood.page.html',
   styleUrls: ['./cartfood.page.scss'],
 })
 export class CartfoodPage implements OnInit {
-
+  listFood=[]
+  listdrinks=[]
+  listdessert=[]
   carrito=[]
-  constructor(private router:Router) { 
+  comida=true;
+  bebida=false;
+  postre=false;
+  constructor(private router:Router,private fire:CloudFirestoreService) { 
+    fire.GetAll('productos').subscribe(data=>{
+      data.forEach(element=>{
+        if(element.type=='postre'){
+          this.listdessert.push(element);
+        }
+        if(element.type=='comida'){
+          this.listFood.push(element);
+        }
+        if(element.type=='bebida'){
+          this.listdrinks.push(element);
+        }
+      })
+    })
+    console.log(this.listdessert)
   }
 
   ngOnInit() {
+  }
+
+  selectFood(){
+    this.postre=false;
+    this.bebida=false;
+    this.comida=true;
+  }
+  selectDrink(){
+    this.comida=false;
+    this.bebida=true;
+    this.postre=false;
+  }
+
+  selectDessert(){
+    this.postre=true;
+    this.bebida=false;
+    this.comida=false;
   }
 
   back(){
