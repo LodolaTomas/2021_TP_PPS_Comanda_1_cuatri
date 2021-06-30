34fc9b92e71ca5aaef1e31486b7d5945139e6669
attrix <lucho.bernal@hotmail.com>
2021-06-30 03:26:05 -0300

    notification service

--- tp_pps/src/app/pages/home-clientes/home-clientes.page.ts
+++ tp_pps/src/app/pages/home-clientes/home-clientes.page.ts
@@ -5,6 +5,7 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { CloudFirestoreService } from 'src/app/services/cloud-firestore.service';
 import Swal, { SweetAlertIcon } from 'sweetalert2';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { NotificationsService } from 'src/app/services/notifications.service';
 
 @Component({
   selector: 'app-home-clientes',
@@ -13,83 +14,106 @@ import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 })
 export class HomeClientesPage implements OnInit {
 
-  navigate : any;
+  navigate: any;
   encodedData: any;
   scannedBarCode: {};
   input: any;
-  cliente:any;
-  displayQREspera:boolean=true;
-  numeroMesa:number=5;
-  displayQRmesa:boolean=true;
-  actionsMesa:boolean=false;
-  carga:boolean=false;
+  cliente: any;
+  displayQREspera: boolean = true;
+  numeroMesa: number = 5;
+  displayQRmesa: boolean = true;
+  actionsMesa: boolean = false;
+  carga: boolean = false;
+
+  public usuarios: any = []
+  public usuarioLog: any = {}
 
   //barcodeScannerOptions: BarcodeScannerOptions;
 
-  constructor(private authS:AuthService, 
-              private router:Router, 
-              private scanner: BarcodeScanner,
-              private fbService:CloudFirestoreService,
-              private localNotifications: LocalNotifications) {
-      this.sideMenu();
-      this.getUser();
+  constructor(private authS: AuthService,
+    private router: Router,
+    private scanner: BarcodeScanner,
+    private fbService: CloudFirestoreService,
+    private notifSVC: NotificationsService) {
+    this.sideMenu();
+    this.getUser();
+
+    fbService.GetAll("usuarios")
+      .subscribe((data) => {
+        this.usuarios = data;
+        this.traerUsuario();
+      });
   }
-  async getUser(){
+
+  async getUser() {
     // this.authS.GetCurrentUser().then((response) => {
     //   let userId =  response.uid;
     // });
 
-      var currentUser = {uid:"edb4z9BC3llciFBhEomB"};
-      // var currentUser = await this.authS.GetCurrentUser();
-      const fbCollection = await this.fbService.GetByParameter("usuarios","id",currentUser.uid).get().toPromise();
-      fbCollection.docs.forEach(d=> this.cliente = d.data());
-      console.log(fbCollection.docs[0].data());
+    var currentUser = { uid: "edb4z9BC3llciFBhEomB" };
+    // var currentUser = await this.authS.GetCurrentUser();
+    const fbCollection = await this.fbService.GetByParameter("usuarios", "id", currentUser.uid).get().toPromise();
+    fbCollection.docs.forEach(d => this.cliente = d.data());
+    console.log(fbCollection.docs[0].data());
   }
   ngOnInit() {
   }
-  sideMenu()
-  {
+  sideMenu() {
     this.navigate =
-    [
-      {
-        title : "Escaneo de Entrada",
-        // url   : "/alta-producto",
-        icon  : "qr-code"
-      }
-      ,
+      [
         {
-          title : "Historial Encuesta",
-          // url   : "/alta-mesa",
-          icon  : "book-outline"
+          title: "Escaneo de Entrada",
+          // url   : "/alta-producto",
+          icon: "qr-code"
         }
-    ]
+        ,
+        {
+          title: "Historial Encuesta",
+          // url   : "/alta-mesa",
+          icon: "book-outline"
+        }
+      ]
   }
-  logout()
-  {
+  logout() {
+    
     this.authS.LogOutCurrentUser()
     this.router.navigateByUrl('login')
   }
+
   openQR() {
-    
+
     console.log("QR!")
     this.scanner.scan().then(res => {
       this.scannedBarCode = res;
       console.log(res);
       let scannedCode = res.text;
-      const userWaitList = { id:this.cliente.id, status:"esperando", date:new Date() };
-      this.fbService.Insert("lista_espera_local",userWaitList)
-                    .then((val)=>{
-                      this.alert('success', "Agregado a la lista de espera!");
-             
-                    });
-      this.displayQREspera=false;
+      const userWaitList = { id: this.cliente.id, status: "esperando", date: new Date() };
+      this.fbService.Insert("lista_espera_local", userWaitList)
+        .then((val) => {
+          this.alert('success', "Agregado a la lista de espera!");
+        });
+      this.displayQREspera = false;
       this.input = this.scannedBarCode["text"];
-      this.notificar({name:'Pepe Anonimo'});
+      this.notifSVC.notifyByProfile("Alguien", "En la lista de espera: ", this.usuarioLog, "admin")
+ //     this.notificar({ name: 'Pepe Anonimo' });
     }).catch(err => {
       alert(err);
     });
 
   }
+
+  async traerUsuario() {
+    this.authS.GetCurrentUser().then((response) => {
+      if (response != null) {
+        let user = this.usuarios.filter((u) => u.email == response.email);
+        this.usuarioLog = user[0];
+        console.log(this.usuarioLog.perfil)
+
+      }
+
+    });
+  }
+
 
   openQRmesa() {
     this.displayQRmesa = false;
@@ -99,14 +123,14 @@ export class HomeClientesPage implements OnInit {
       this.scannedBarCode = res;
       console.log(res);
       let scannedCode = res.text;
-      const userWaitList = { id:this.cliente.id, status:"esperando", date:new Date() };
-      this.fbService.Insert("lista_espera_local",userWaitList)
-                    .then((val)=>{
-                    this.displayQRmesa = false;
-                    this.actionsMesa = true;
-                    this.carga = false;
-                    });
-    
+      const userWaitList = { id: this.cliente.id, status: "esperando", date: new Date() };
+      this.fbService.Insert("lista_espera_local", userWaitList)
+        .then((val) => {
+          this.displayQRmesa = false;
+          this.actionsMesa = true;
+          this.carga = false;
+        });
+
       this.input = this.scannedBarCode["text"];
 
     }).catch(err => {
@@ -115,16 +139,10 @@ export class HomeClientesPage implements OnInit {
 
   }
 
-  notificar(user: any) {
-    this.localNotifications.schedule({
-      id: 1,
-      text: 'Un cliente en la lista de espera!: ' + user.name,
-      sound: 'file://android/app/src/main/res/raw/sound.mp3',
-    });
 
 
-  }
 
+  
   alert(icon: SweetAlertIcon, text: string) {
     const Toast = Swal.mixin({
       toast: true,
@@ -144,7 +162,7 @@ export class HomeClientesPage implements OnInit {
       title: text
     })
   }
-  cartfood(){
+  cartfood() {
     this.router.navigateByUrl('cartfood')
   }
 
